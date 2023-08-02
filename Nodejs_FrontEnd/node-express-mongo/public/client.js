9c50ef90cfb4fde154365564516c5a44f99004fc
Certified Humane <castrolwilliam@gmail.com>
2023-08-02 12:26:15 -0400

    working on a online demo
    This demo detects the button being press on the web page

---
+++ Nodejs_FrontEnd/node-express-mongo/public/client.js
@@ -0,0 +1,7 @@
+console.log("Client side code running");
+
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e){
+    console.log('button Was clicked' );
+});
+
