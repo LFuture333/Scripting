16054f7fff64b7250a222d7a795867ec271f50af
Certified Humane <castrolwilliam@gmail.com>
2023-08-03 04:51:27 -0400

    f

--- Nodejs_FrontEnd/node-express-mongo/public/client.js
+++
@@ -1,7 +0,0 @@
-console.log("Client side code running");
-
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e){
-    console.log('button Was clicked' );
-});
-
---
+++ mysql/connect_mysql.py
@@ -0,0 +1,11 @@
+import mysql.connector 
+
+
+mydb = mysql.connector.connect(
+	host ="localhost",
+	user ="access_ai",
+	password = "T$4kgndsngsg(%"	
+)
+
+
+print(mydb)
