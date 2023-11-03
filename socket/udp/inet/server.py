1e7233ee7ee5e6120496bab39dce7a1ab63b3a8d
Louis Future <140074813+LFuture333@users.noreply.github.com>
2023-11-03 16:19:17 -0400

    Update server.py

--- socket/udp/inet/server.py
+++ socket/udp/inet/server.py
@@ -1,1 +1,7 @@
 import socket
+
+https://stackoverflow.com/questions/61448823/how-to-handle-udp-socket-clients-in-python
+
+https://www.studytonight.com/network-programming-in-python/working-with-udp-sockets
+
+https://akuchhattri14.medium.com/basics-of-socket-programming-in-python-using-udp-protocol-4e08386a6d75
